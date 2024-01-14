2fd4999f949c95eedd185eb7cd017bb3fd925e2e
RG <goyalrohan2020@gmail.com>
2024-01-14 20:30:42 +0530

    feat: internal marks fetching

---
+++ script/internal_marks.py
@@ -0,0 +1,67 @@
+from prettytable import PrettyTable
+
+from selenium.webdriver.support.select import Select
+from util.prompter import prompt
+from inquirer.questions import List
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import TimeoutException
+
+INTERNAL_MARKS_URL = "https://mujslcm.jaipur.manipal.edu:122/Student/Academic/InternalMarkForStudent"
+
+
+def gather_internal_marks(browser):
+    browser.get(INTERNAL_MARKS_URL)
+
+    def select_semester():
+        select_tag = browser.find_element("id", "ddlSemester")
+        option_tags = select_tag.find_elements("tag name", "option")
+        options = []
+        for option in option_tags:
+            options.append((option.get_attribute("value"), option.text))
+        selected_option = prompt(List(
+            name="semester",
+            message="Please select your semester",
+            choices=map(lambda x: x[1], options),
+            carousel=True
+        ))
+        selected_option_value = None
+        for option in options:
+            if option[1] == selected_option:
+                selected_option_value = option[0]
+                break
+        select_clickable = Select(select_tag)
+        select_clickable.select_by_value(selected_option_value)
+        browser.find_element("id", "btnSearch").click()
+
+    def get_marks():
+        try:
+            WebDriverWait(browser, 3).until(ec.alert_is_present(),
+                                           'Timed out waiting for PA creation ' +
+                                           'confirmation popup to appear.')
+
+            alert = browser.switch_to.alert
+            alert.accept()
+            print("No record found")
+            return
+        except TimeoutException:
+            print("Loading marks...")
+        table = browser.find_element("id", "kt_ViewTable")
+        if table is None:
+            print("No marks found")
+            return
+        table_heads = (table.find_elements("tag name", "thead").find_element("tag name", "tr")
+                       .find_elements("tag name", "th"))
+        display = PrettyTable()
+        display.field_names = [head.text for head in table_heads]
+        table_rows = table.find_elements("tag name", "tr")
+        for row in table_rows:
+            cols = row.find_elements("tag name", "td")
+            array = []
+            for col in cols:
+                array.append(col.text)
+            display.add_row(array)
+        print(display)
+
+    select_semester()
+    get_marks()
