7cf9574a0c10c96e5d079b3a8cf79d0d87371fc9
RG <goyalrohan2020@gmail.com>
2024-01-14 20:07:10 +0530

    feat: attendance checking

---
+++ script/attendance.py
@@ -0,0 +1,20 @@
+from prettytable import PrettyTable
+
+ATTENDANCE_URL = "https://mujslcm.jaipur.manipal.edu:122/Student/Academic/AttendanceSummaryForStudent"
+
+
+def gather_attendance(browser):
+    browser.get(ATTENDANCE_URL)
+    table = browser.find_element("id", "kt_ViewTable")
+    table_heads = (table.find_elements("tag name", "thead").find_element("tag name", "tr")
+                   .find_elements("tag name", "th"))
+    display = PrettyTable()
+    display.field_names = [head.text for head in table_heads]
+    table_rows = table.find_elements("tag name", "tr")
+    for row in table_rows:
+        cols = row.find_elements("tag name", "td")
+        array = []
+        for col in cols:
+            array.append(col.text)
+        display.add_row(array)
+    print(display)
